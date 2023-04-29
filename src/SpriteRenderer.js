dc97ab91d024033909082805084c7efbd7ee3b36
The-Good-Goomba <86233119+The-Good-Goomba@users.noreply.github.com>
2023-04-29 20:42:09 +1000

    2D drawing

--- src/SpriteRenderer.js
+++ src/SpriteRenderer.js
@@ -20,11 +20,14 @@ class SpriteRenderer
 
     updateInstances()
     {
-        this.#instanceValues = new Float32Array(this.sprites.length * 9);
+        this.#instanceValues = new ArrayBuffer(4 * 9 * this.sprites.length)
+        let floatArray =   new Float32Array(this.#instanceBuffer);
+        let uintArray =   new Uint32Array(this.#instanceBuffer);
         let i = 0
         for (let sprite in this.sprites)
         {
-            this.#instanceValues.set(sprite.instanceData, i);
+            floatArray.set(sprite.instanceData, i);
+            uintArray.set(sprite.sprite.textureType, i + 8);
             i += 9;
         }
         Main.device.queue.writeBuffer(this.#instanceBuffer, 0, this.#instanceValues);
