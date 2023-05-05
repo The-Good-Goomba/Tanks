882cfc8d6653cf36dd20f06f3cffa8103c044a28
The-Good-Goomba <86233119+The-Good-Goomba@users.noreply.github.com>
2023-05-05 14:19:31 +1000

    Refactor Scene

--- src/Apex/Scene/Scene.js
+++ src/Apex/Scene/Scene.js
@@ -1,5 +1,21 @@
 
 class Scene extends Apex
 {
+    spriteRenderer;
+
+    constructor()
+    {
+        super("Scene");
+        this.toRender = false;
+        mat4.identity(this.viewMatrix);
+        mat4.identity(this.projectionMatrix);
+        this.spriteRenderer = new SpriteRenderer();
+
+    }
+
+    render(renderCommandEncoder) {
+        super.render(renderCommandEncoder);
+        this.spriteRenderer.doRender(renderCommandEncoder);
+    }
 
 }
