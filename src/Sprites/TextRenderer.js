864f9b995a7a01a8f33e7b65be78bcec5742995f
The-Good-Goomba <86233119+The-Good-Goomba@users.noreply.github.com>
2023-05-24 12:15:53 +1000

    Text Renderer Continued...

--- src/Sprites/TextRenderer.js
+++ src/Sprites/TextRenderer.js
@@ -2,15 +2,15 @@
 class TextRenderer extends SpriteRenderer
 {
     #texture;
-    #textCanvas;
+    #canvas;
+    #canvasContext;
 
     constructor() {
         super();
-        const canvas = new OffscreenCanvas(128,128);
-        this.#textCanvas = canvas.getContext("2d");
-        this.#textCanvas.textAlign = "center";
-        this.#textCanvas.textBaseline = "middle";
-        console.log(this.#textCanvas.width);
+        this.#canvas = new OffscreenCanvas(128,128);
+        this.#canvasContext = this.#canvas.getContext("2d");
+        this.#canvasContext.textAlign = "center";
+        this.#canvasContext.textBaseline = "middle";
     }
 
 
@@ -21,10 +21,10 @@ class TextRenderer extends SpriteRenderer
         let newWidth = Math.min(len * 128, 4 * 128);
         let newHeight = Math.ceil(len / 5) * 128;
 
-        if (newWidth === this.#textCanvas.width && newHeight === this.#textCanvas.height) return;
+        if (newWidth === this.#canvas.width && newHeight === this.#canvas.height) return;
 
         this.#texture = Main.device.createTexture({
-            size: [ this.#textCanvas.width,  this.#textCanvas.height, 1 ],
+            size: [ this.#canvas.width,  this.#canvas.height, 1 ],
             format: Main.colourFormat,
             usage: GPUTextureUsage.COPY_DST | GPUTextureUsage.TEXTURE_BINDING | GPUTextureUsage.RENDER_ATTACHMENT
         });
@@ -47,7 +47,7 @@ class TextRenderer extends SpriteRenderer
 
         // Draw all the things
         // Link: https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/fillText
-        this.#textCanvas.clearRect(0,0,this.#textCanvas.width,this.#textCanvas.height);
+        this.#canvasContext.clearRect(0,0,this.#canvas.width,this.#canvas.height);
 
         for (let i = 0; i < this._sprites.length; i++)
         {
@@ -55,21 +55,21 @@ class TextRenderer extends SpriteRenderer
 
             sus.sprite = { pos: [(i % 5) * 128,Math.floor(i / 5) * 128], size: [128,128] };
 
-            this.#textCanvas.font = `${sus.fontSize}px "${sus.font}"`;
-            this.#textCanvas.fillText(sus.text,...sus.pos, 128);
+            this.#canvasContext.font = `${sus.fontSize}px "${sus.font}"`;
+            this.#canvasContext.fillText(sus.text,sus.sprite.pos[0],sus.sprite.pos[1], 128);
 
-            sus.pos[0]  /=  this.#textCanvas.width;
-            sus.pos[1]  /=  this.#textCanvas.height;
-            sus.size[0] /= this.#textCanvas.width;
-            sus.size[1] /= this.#textCanvas.height;
+            sus.sprite.pos[0]  /=  this.#canvas.width;
+            sus.sprite.pos[1]  /=  this.#canvas.height;
+            sus.sprite.size[0] /= this.#canvas.width;
+            sus.sprite.size[1] /= this.#canvas.height;
         }
 
 
         // Then copy
         Main.device.queue.copyExternalImageToTexture(
-            { source: this.#textCanvas },
+            { source: this.#canvas },
             { texture: this.#texture },
-            [this.#textCanvas.width, this.#textCanvas.height]
+            [this.#canvas.width, this.#canvas.height]
         );
         super.updateInstances();
     }
